fc7cf92eb79b73132eeb1c89fc13e6810ecd597f
Jose Ocampo <jose.ocampo@paymentevolution.com>
2026-04-14 10:45:10 -0500

    feat: add version parameter to ObjectCountingModel for model selection

--- computer_vision/object_counting/models.py
+++ computer_vision/object_counting/models.py
@@ -1,24 +1,33 @@
-import torch 
-import torch.nn as nn 
-import torchvision.models as models 
-from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_ResNet50_FPN_V2_Weights
+import torch.nn as nn
+import torchvision.models as models
+from torchvision.models.detection import (
+    fasterrcnn_resnet50_fpn_v2,
+    fasterrcnn_resnet50_fpn,
+    FasterRCNN_ResNet50_FPN_V2_Weights,
+    FasterRCNN_ResNet50_FPN_Weights,
+)
+
 
 class ObjectCountingModel(nn.Module):
-    def __init__(self, num_classes=2): # [0 background, 1 person]
+    def __init__(self, num_classes=2, version="v2"):  # [0 background, 1 person]
         super(ObjectCountingModel, self).__init__()
 
-        self.weights = FasterRCNN_ResNet50_FPN_V2_Weights.COCO_V1
-        self.model = fasterrcnn_resnet50_fpn_v2(weights=self.weights)
+        if version == "v2":
+            self.weights = FasterRCNN_ResNet50_FPN_V2_Weights.COCO_V1
+            self.model = fasterrcnn_resnet50_fpn_v2(weights=self.weights)
+        else:
+            self.weights = FasterRCNN_ResNet50_FPN_Weights.COCO_V1
+            self.model = fasterrcnn_resnet50_fpn(weights=self.weights)
 
         for param in self.model.parameters():
             param.requires_grad_(False)
 
         in_features = self.model.roi_heads.box_predictor.cls_score.in_features
 
-        self.model.roi_heads.box_predictor = models.detection.faster_rcnn.FastRCNNPredictor(
-            in_features, num_classes
+        self.model.roi_heads.box_predictor = (
+            models.detection.faster_rcnn.FastRCNNPredictor(in_features, num_classes)
         )
-    
+
     def forward(self, images, targets=None):
         if self.training and targets is not None:
             return self.model(images, targets)
